e5572260696c7382ffe4eddbe879e92bc6d57051
pavel borisov <frailty.nine@gmail.com>
2023-10-30 13:05:03 +0000

    Debug statement fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
